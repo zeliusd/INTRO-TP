1552d071c656e855bb0ecb059ae884b1492dc9d2
zeliusd <mzorrilla@fi.uba.ar>
2024-06-19 20:52:31 -0300

    cambios en gitignore

---
+++ panda.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+df = pd.read_csv("games.csv")
+
+engine = create_engine(
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/peliculas"
+)
+
+df_100 = df.head(100)
+df_100.to_sql("games", engine, if_exists="replace", index=False)
+
+print("Datos guardados en la base de datos con éxito")
