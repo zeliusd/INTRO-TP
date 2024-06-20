4275423e44c4fac4f9fb42dc22e5cc6a99903f6e
zeliusd <mzorrilla@fi.uba.ar>
2024-06-20 00:54:04 -0300

    Arreglando dependencias

--- panda.py
+++ panda.py
@@ -3,9 +3,7 @@ from sqlalchemy import create_engine
 
 df = pd.read_csv("games.csv")
 
-engine = create_engine(
-    "postgresql+psycopg2://postgres:postgres@localhost:5432/peliculas"
-)
+engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/games")
 
 df_100 = df.head(100)
 df_100.to_sql("games", engine, if_exists="replace", index=False)
