3f74f7871eb7667535e75772fc5ccfb21739f52c
zeliusd <mzorrilla@fi.uba.ar>
2024-06-21 00:51:33 -0300

    Se agregaron los endpoints del backend filter, games, games/id/

---
+++ api/tables/games.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Games(db.Model):
+    __tablename__ = "games"
+    appid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    about_the_game = db.Column(db.Text, nullable=False)
+    developers = db.Column(db.String(255), nullable=False)
+    header_image = db.Column(db.String(255), nullable=False)
+    categories = db.Column(db.String(255), nullable=False)
