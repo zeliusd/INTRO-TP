614e41426bd4359b3df75587dc5519516082d260
zeliusd <mzorrilla@fi.uba.ar>
2024-07-09 20:24:22 -0300

    Test: añadiendo todo los datos del csv

--- api/db/panda.py
+++ api/db/panda.py
@@ -3,15 +3,15 @@ import pandas
 from sqlalchemy import create_engine
 
 os.system("unzip db/games_data.zip")
-df = pandas.read_csv("games.csv")
+data_frame = pandas.read_csv("games.csv")
 
 engine = create_engine("postgresql+psycopg2://postgres:postgres@db:5432/games_db")
 
-df.columns = df.columns.str.replace(" ", "_").str.lower()
+data_frame.columns = data_frame.columns.str.replace(" ", "_").str.lower()
 
-df_100 = df.head(100)
+# df_100 = df.head(100)
 
-df_100.to_sql("games_tb", engine, if_exists="replace", index=False)
+data_frame.to_sql("games_tb", engine, if_exists="replace", index=False)
 
 print("Datos guardados en la base de datos con éxito")
 
