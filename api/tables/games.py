844c327c2fec2aab0ff9f64a1db94473e3c53bf9
Emi <efernandezt@fi.uba.ar>
2024-07-08 22:27:53 -0300

    add tables Users and Reviews

--- api/tables/games.py
+++ api/tables/games.py
@@ -12,3 +12,18 @@ class Games(db.Model):
     developers = db.Column(db.String(255), nullable=False)
     header_image = db.Column(db.String(255), nullable=False)
     categories = db.Column(db.String(255), nullable=False)
+
+class Users(db.Model):
+    __tablename__ = "users_tb"
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    cant_reviews = db.Column(db.Integer, nullable=True)
+
+class Review(db.Model):
+    __tablename__ = "review_tb"
+    review_id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(1000), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    appid = db.Column(db.ForeignKey('games_tb.appid') ,nullable=False)
+    user_id = db.Column(db.ForeignKey('users_tb.user_id') ,nullable=False)
